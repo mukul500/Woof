851b5906dfef276955ee6cb3f001a36ec60cdfe4
Mukul <mukul@geeksforgeeks.org>
2021-03-03 01:11:45 +0530

    Updated Strings and Icons

--- app/src/main/java/net/xblacky/woof/component/TopBar.kt
+++ app/src/main/java/net/xblacky/woof/component/TopBar.kt
@@ -1,16 +1,13 @@
 package net.xblacky.woof.component
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.woof.R
 
 
 @Preview
@@ -39,26 +36,5 @@ fun TopBar() {
             )
         }
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 20.dp, 24.dp, 0.dp),
-            horizontalArrangement = Arrangement.End
-        ) {
-            WoofDarkMode()
-        }
-
-
     }
-}
-
-@Composable
-fun WoofDarkMode() {
-    val icon = painterResource(id = R.drawable.ic_sun)
-
-    Icon(
-        painter = icon,
-        contentDescription = null,
-        modifier = Modifier.size(24.dp, 24.dp)
-    )
 }
